337a9c94d9301eb8cdd31bb32076237bbc2292c2
josephr04 <jrosas.dev@gmail.com>
2025-01-18 23:29:33 -0500

    Fix page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import styles from "./page.module.css";
+import Header from "./Header"
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <main>
-        <header>
-          <nav>
-
-          </nav>
-        </header>
         <h1 className="text-danger">Movie App</h1>
       </main>
       <footer>
