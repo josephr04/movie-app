365da7572e910a639c0c41d52e2020b8b5cf1571
josephr04 <jrosas.dev@gmail.com>
2025-01-19 23:50:46 -0500

    Add list elements to nav

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import NavListItems from './components/header/NavListItems'
 
 function Header() {
   return (
-    <div>header</div>
+    <header>
+      <a href="/" className='logo'>
+      </a>
+      <ul className='nav'>
+        <NavListItems name="Upcoming"/>
+        <NavListItems name="Popular"/>
+        <NavListItems name="Categories"/>
+      </ul>
+    </header>
   )
 }
 
