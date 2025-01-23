4bfc1f3e0ab2d457a1552eae90ef340034d33062
josephr04 <jrosas.dev@gmail.com>
2025-01-22 22:43:46 -0500

    Fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Header from "./Header"
 import { Geist, Geist_Mono } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./globals.css";
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header/>
         {children}
       </body>
     </html>
