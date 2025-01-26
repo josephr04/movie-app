56e28d82e234a33b7682ee784699c79c2b7a22cb
josephr04 <jrosas.dev@gmail.com>
2025-01-25 23:00:22 -0500

    Fix main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import Header from "./Header"
 export default function Home() {
   return (
     <div>
-      <Header/>
       <main>
-        <h1 className="text-danger">Movie App</h1>
       </main>
       <footer>
       </footer>
