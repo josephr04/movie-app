c455cf83fc9b763554974791a556b3844fed2b9a
josephr04 <jrosas.dev@gmail.com>
2025-02-07 22:56:53 -0500

    Removing movie div

--- app/components/home/banner.tsx
+++ app/components/home/banner.tsx
@@ -75,36 +75,34 @@ export async function Banner() {
 
   return (
     <div className={styles.banner}>
-      <div className={styles.movie}>
-        <div id="movieCarousel" className="carousel slide" data-bs-ride="carousel">
-          <div className={`carousel-indicators ${styles.carouselIndicators}`}>
-            {posts.results.slice(0, 6).map((_, index) => (
-              <button key={index} type="button" data-bs-target="#movieCarousel" data-bs-slide-to={index} className={index === 0 ? "active" : ""} aria-current="true" aria-label={`Slide ${index + 1}`}></button>
-            ))}
-          </div>
-          <div className="carousel-inner">
-            {posts.results.slice(0, 6).map((movie, index) => (
-              <div key={movie.id} className={`carousel-item ${index === 0 ? "active" : ""}`} data-bs-interval={index === 0 ? 10000 : 2000}>
-                <div className={styles.imgOverlay}></div>  
-                <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="d-block w-100" alt={movie.title} />
-                <div className={`carousel-caption d-none d-md-block ${styles.movieInfo}`}>
-                  <h3>{movie.title}</h3>
-                  <p className={styles.movieGenres}>{movie.genre_ids.map((id) => genreMap[id]).join(", ")}</p>
-                  <p>{movie.overview}</p>
-                  <button type="button" className="btn btn-warning">Watch more</button>
-                </div>
-              </div>
-            ))}
-          </div>
-          <button className={`carousel-control-prev ${styles.carouselButton}`} type="button" data-bs-target="#movieCarousel" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className={`carousel-control-next ${styles.carouselButton}`} type="button" data-bs-target="#movieCarousel" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+      <div id="movieCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div className={`carousel-indicators ${styles.carouselIndicators}`}>
+          {posts.results.slice(0, 6).map((_, index) => (
+            <button key={index} type="button" data-bs-target="#movieCarousel" data-bs-slide-to={index} className={index === 0 ? "active" : ""} aria-current="true" aria-label={`Slide ${index + 1}`}></button>
+          ))}
         </div>
+        <div className="carousel-inner">
+          {posts.results.slice(0, 6).map((movie, index) => (
+            <div key={movie.id} className={`carousel-item ${index === 0 ? "active" : ""}`} data-bs-interval={index === 0 ? 10000 : 2000}>
+              <div className={styles.imgOverlay}></div>  
+              <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="d-block w-100" alt={movie.title} />
+              <div className={`carousel-caption d-none d-md-block ${styles.movieInfo}`}>
+                <h3>{movie.title}</h3>
+                <p className={styles.movieGenres}>{movie.genre_ids.map((id) => genreMap[id]).join(", ")}</p>
+                <p>{movie.overview}</p>
+                <button type="button" className="btn btn-warning">Watch more</button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button className={`carousel-control-prev ${styles.carouselButton}`} type="button" data-bs-target="#movieCarousel" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className={`carousel-control-next ${styles.carouselButton}`} type="button" data-bs-target="#movieCarousel" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
