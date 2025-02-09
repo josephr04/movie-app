c273bc67fdc90c0e217715c78fe7d16466cd9f5c
josephr04 <jrosas.dev@gmail.com>
2025-02-08 22:35:56 -0500

    Add CardCarousel components to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import { Suspense } from "react";
 import { Banner } from "./components/home/banner";
 import { Loading } from "./components/Loading";
+import { CardCarousel } from "./components/home/CardCarousel"
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div>
+      <main>
       <Suspense fallback={<Loading />}>
         <Banner/>
       </Suspense>
-      <main>
+      <div className={styles.cardSection}>
+        <CardCarousel title="Now Playing" category="now_playing"/>
+        <CardCarousel title="Popular" category="popular"/>
+        <CardCarousel title="Top Rated" category="top_rated"/>
+        <CardCarousel title="Upcoming" category="upcoming"/>
+      </div>
       </main>
       <footer>
       </footer>
