5903da96f7521f127872898c9862fdab0ab99088
josephr04 <jrosas.dev@gmail.com>
2025-02-10 20:20:15 -0500

    Add backtotop button

---
+++ app/components/BackToTop.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import styles from '../page.module.css';
+import { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+export function BackToTop() {
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowButton(window.scrollY > 300);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <button 
+            className={`${styles.backToTop} ${showButton ? styles.show : styles.hide}`} 
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
+            <FaArrowUp />
+        </button>
+    );
+}
