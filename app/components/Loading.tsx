666a8ca30621f0b61f7cf8072d4c903b29607cb2
josephr04 <jrosas.dev@gmail.com>
2025-02-11 22:43:11 -0500

    Fix Loading component style

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,8 +1,12 @@
+import styles from '../page.module.css';
+
 export function Loading() {
     return (
-      <div className={`d-flex justify-content-center align-items-center`} style={{ height: '100vh' }}>
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
+      <div className={styles.loading}>
+        <div className={`d-flex justify-content-center align-items-center`}>
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
         </div>
       </div>
     );
