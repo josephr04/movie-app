3f8cf24964adf3bc194f2221982e5f1699084e13
josephr04 <jrosas.dev@gmail.com>
2025-02-27 22:41:04 -0500

    Fix nav list items links

--- app/components/header/NavListItems.tsx
+++ app/components/header/NavListItems.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 import { NavItems } from '@/app/data/NavListData';
 import styles from '../../page.module.css';
 
 interface NavListItemsProps {
   nav: NavItems;
+  activeClass: string;
 }
 
-export function NavListItems({ nav }: NavListItemsProps) {
+export function NavListItems({ nav, activeClass }: NavListItemsProps) {
   return (
-    <li className={styles.nav}>
-      <a href={nav.link} className={styles['nav-link']}>{nav.name}</a>
-    </li>
-  )
+    <Link href={nav.link} passHref>
+      <li className={`${styles.navListItems} ${activeClass}`} style={{ cursor: 'pointer' }}>
+        {nav.name}
+      </li>
+    </Link>
+  );
 }
