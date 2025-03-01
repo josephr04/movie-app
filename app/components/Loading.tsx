f4f0964e351742c2f7a14cbfbb68637a3c372851
josephr04 <jrosas.dev@gmail.com>
2025-03-01 18:17:45 -0500

    Add new Loading animation component

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -11,3 +11,15 @@ export function Loading() {
       </div>
     );
 }
+
+export function LoadingItems() {
+  return (
+    <div className={styles.loadingItems}>
+      <div className={`d-flex justify-content-center align-items-center`}>
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </div>
+  );
+}
