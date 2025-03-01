384fb295a74384acbd363558453c42ff6a7b655d
josephr04 <jrosas.dev@gmail.com>
2025-02-28 19:03:53 -0500

    Add NavList file to handle active styles on header items

---
+++ app/components/header/NavList.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import { navListData } from '../../data/NavListData';
+import { NavListItems } from './NavListItems';
+import styles from '../../page.module.css';
+
+export function NavList() {
+  const pathname = usePathname();
+
+  return (
+    <ul className={styles.navList}>
+      {navListData.map((nav) => {
+        const isActive = pathname === nav.link;
+        return (
+          <NavListItems
+            key={nav.id}
+            nav={nav}
+            activeClass={isActive ? styles.activeNav : ''}
+          />
+        );
+      })}
+    </ul>
+  );
+}
