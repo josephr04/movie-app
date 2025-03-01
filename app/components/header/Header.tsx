e121a418b0ace211848e7daa95ee0940bef1639c
josephr04 <jrosas.dev@gmail.com>
2025-02-28 19:08:57 -0500

    Fix header links and overall display

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { NavListItems } from './NavListItems';
-import { navListData } from '../../data/NavListData';
+import Link from 'next/link';
 import { Search } from 'lucide-react';
 import { DynamicHeader } from "../Animations";
+import { NavList } from './NavList';
+import { Ripple } from 'primereact/ripple';
 import styles from '../../page.module.css';
 
 function Header() {
   return (
     <DynamicHeader>
-      <a href="/">
-        <h1 className={styles.logo}>Movies+</h1>
-      </a>
-      <ul className={styles.nav}>
-        {
-          navListData.map(nav => (
-            <NavListItems key={nav.id} nav={nav} />
-          ))
-        }
-      </ul>
-      <a href="/search" className={styles.search}>
+      <Link href="/" className={styles.logoLink} id={styles.test}>
+        <h1 className={styles.logo} >Movies+</h1>
+      </Link>
+      <NavList />
+      <Link href="/search" className={`${styles.search} p-ripple`}>
         <Search />
-      </a>
+        <Ripple />
+      </Link>
     </DynamicHeader>
-  )
+  );
 }
 
-export default Header
+export default Header;
