25bae6e950b989e98e6c5b5b3d6f8db292240a72
josephr04 <jrosas.dev@gmail.com>
2025-03-03 12:13:09 -0500

    Add GetMovies component to popular page

--- app/popular/page.tsx
+++ app/popular/page.tsx
@@ -1,24 +1,6 @@
 import MovieList from "../components/MovieList";
+import { getMovies } from "../components/GetMovies"
 import styles from "../page.module.css";
-
-async function getMovies(page: number) {
-  const url = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}`;
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
-    },
-  };
-
-  const res = await fetch(url, options);
-  if (!res.ok) {
-    throw new Error("Failed to fetch movies");
-  }
-
-  const data = await res.json();
-  return data.results;
-}
 
 export default async function Page() {
   const initialMovies = await getMovies(1);
