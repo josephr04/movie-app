1245c6ce7b0846c86962977ac5829e165c4014d3
josephr04 <jrosas.dev@gmail.com>
2025-03-04 21:38:59 -0500

    Moving movie components to their folder

--- app/components/movies/SearchMovies.tsx
+++ app/components/movies/SearchMovies.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { MovieCard } from "./movies/MovieCard";
+import { MovieCard } from "./MovieCard";
 import styles from "../page.module.css";
 
 interface Movie {
