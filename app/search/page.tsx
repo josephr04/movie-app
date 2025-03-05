f6ba2b966f98d6348d11a76cbba6eca43fe95651
josephr04 <jrosas.dev@gmail.com>
2025-03-04 21:12:59 -0500

    Fix loading states and change movie fetch component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,14 +5,22 @@ import Input from '@mui/material/Input';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import CircularProgress from '@mui/material/CircularProgress';
 import styles from "../page.module.css";
-import { SearchResults } from "../components/SearchResults";
+import { SearchMovies } from "../components/SearchMovies";
 
 export default function Page() {  
     const [searchTerm, setSearchTerm] = useState("");
     const [debouncedSearch, setDebouncedSearch] = useState("");
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setDebouncedSearch("");
+
+        if (!searchTerm) {
+            setLoading(false);
+        }
+
         const handler = setTimeout(() => {
             setDebouncedSearch(searchTerm);
         }, 500);
@@ -28,11 +36,11 @@ export default function Page() {
                     sx={{ width: 800, color: 'white', fontSize: 40 }} 
                     className={styles.searchInput} 
                     value={searchTerm} 
-                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onChange={(e) => { setSearchTerm(e.target.value); setLoading(true);}}
                     endAdornment={
                         searchTerm && (
                             <InputAdornment position="end">
-                                <IconButton onClick={() => setSearchTerm("")} edge="end">
+                                <IconButton onClick={() => { setSearchTerm(""); setLoading(false);}} edge="end">
                                     <CloseIcon sx={{ color: "white", fontSize: 30 }} />
                                 </IconButton>
                             </InputAdornment>
@@ -41,7 +49,19 @@ export default function Page() {
                 />
             </div>
             <div>
-                <SearchResults searchTerm={debouncedSearch}/>
+                {!searchTerm && (
+                    <div className={styles.searchLoading}>
+                        <h2>Type something to search...</h2>
+                    </div>
+                )}
+                {debouncedSearch && (
+                    <SearchMovies name={debouncedSearch} setLoading={setLoading} />
+                )}
+                {loading && (
+                    <div className={styles.searchLoading}>
+                        <CircularProgress color="inherit" size={50}/>
+                    </div>
+                )}
             </div>
         </div>
     );
