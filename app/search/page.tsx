c34ea0623e65dd4e75ba169764e61115cab2c9ea
josephr04 <jrosas.dev@gmail.com>
2025-03-05 21:06:01 -0500

    Fix imports and identation in search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,62 +7,62 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import CircularProgress from '@mui/material/CircularProgress';
 import styles from "../page.module.css";
-import { SearchMovies } from "../components/movies/SearchMovies";
+import { SearchMovies } from "@components/movies/SearchMovies";
 
 export default function Page() {  
-    const [searchTerm, setSearchTerm] = useState("");
-    const [debouncedSearch, setDebouncedSearch] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setDebouncedSearch("");
+  useEffect(() => {
+    setDebouncedSearch("");
 
-        if (!searchTerm) {
-            setLoading(false);
-        }
+    if (!searchTerm) {
+      setLoading(false);
+    }
 
-        const handler = setTimeout(() => {
-            setDebouncedSearch(searchTerm);
-        }, 500);
+    const handler = setTimeout(() => {
+      setDebouncedSearch(searchTerm);
+    }, 500);
 
-        return () => clearTimeout(handler);
-    }, [searchTerm]);
+    return () => clearTimeout(handler);
+  }, [searchTerm]);
 
-    return (
-        <div className={styles.searchContainer}>
-            <div className={styles.searchHeader}>
-                <Input 
-                    placeholder="Search..."  
-                    sx={{ width: 800, color: 'white', fontSize: 40 }} 
-                    className={styles.searchInput} 
-                    value={searchTerm} 
-                    onChange={(e) => { setSearchTerm(e.target.value); setLoading(true);}}
-                    endAdornment={
-                        searchTerm && (
-                            <InputAdornment position="end">
-                                <IconButton onClick={() => { setSearchTerm(""); setLoading(false);}} edge="end">
-                                    <CloseIcon sx={{ color: "white", fontSize: 30 }} />
-                                </IconButton>
-                            </InputAdornment>
-                        )
-                    }
-                />
-            </div>
-            <div>
-                {!searchTerm && (
-                    <div className={styles.searchLoading}>
-                        <h2>Type something to search...</h2>
-                    </div>
-                )}
-                {debouncedSearch && (
-                    <SearchMovies name={debouncedSearch} setLoading={setLoading} />
-                )}
-                {loading && (
-                    <div className={styles.searchLoading}>
-                        <CircularProgress color="inherit" size={50}/>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.searchContainer}>
+      <div className={styles.searchHeader}>
+        <Input 
+          placeholder="Search..."  
+          sx={{ width: 800, color: 'white', fontSize: 40 }} 
+          className={styles.searchInput} 
+          value={searchTerm} 
+          onChange={(e) => { setSearchTerm(e.target.value); setLoading(true);}}
+          endAdornment={
+            searchTerm && (
+              <InputAdornment position="end">
+                <IconButton onClick={() => { setSearchTerm(""); setLoading(false);}} edge="end">
+                  <CloseIcon sx={{ color: "white", fontSize: 30 }} />
+                </IconButton>
+              </InputAdornment>
+            )
+          }
+        />
+      </div>
+      <div>
+        {!searchTerm && (
+          <div className={styles.searchLoading}>
+            <h2>Type something to search...</h2>
+          </div>
+        )}
+        {debouncedSearch && (
+          <SearchMovies name={debouncedSearch} setLoading={setLoading} />
+        )}
+        {loading && (
+          <div className={styles.searchLoading}>
+            <CircularProgress color="inherit" size={50}/>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
