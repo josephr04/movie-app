7950bde648088ffd4589ed4ea30b7e5fd17eb89b
josephr04 <jrosas.dev@gmail.com>
2025-03-06 22:51:50 -0500

    Fix Movie Cards width bug

--- app/components/movies/MovieCard.tsx
+++ app/components/movies/MovieCard.tsx
@@ -22,7 +22,7 @@ export function MovieCard({ movie }: MovieCardProps) {
   const slug = slugify(movie.title, { lower: true });
 
   return (
-    <Link href={`/movies/${slug}`} className={`text-center mx-3 ${styles.movieCard} p-ripple`}>
+    <Link href={`/movies/${slug}`} className={`text-center mx-3 w-100 ${styles.movieCard} p-ripple`}>
         <div className={styles.movieOverlay}>
           <p className={styles.movieTitleOverlay}>{movie.title}</p>
           <div className={styles.movieRating}>
