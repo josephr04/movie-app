2fa64999c6a0e2dac3197b9d471f2806c4e2f906
josephr04 <jrosas.dev@gmail.com>
2025-03-14 23:37:10 -0500

    Fix movie header and information style in movie page

--- app/movies/[id]/[slug]/page.tsx
+++ app/movies/[id]/[slug]/page.tsx
@@ -1,5 +1,9 @@
-import styles from '../../../page.module.css';
 import slugify from "slugify";
+import Image from "next/image";
+import tmdbLogo from "@assets/tmdb-logo.png";
+import Link from 'next/link';
+import LanguageIcon from '@mui/icons-material/Language';
+import styles from '../../../page.module.css';
 
 interface PageProps {
   params : {
@@ -29,33 +33,38 @@ async function getMovieData(slug: string) {
 
 export default async function page({ params }: PageProps) {
   const movie = await getMovieData(params.id);
+  const rating = parseFloat(movie.vote_average).toFixed(1);
 
   return (
     <div>
-      <div className={styles.movieHeader}>
-        <div className={styles.moviePageInfo}>
-          <div className={styles.moviePoster}>
-            <img
-              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-              alt={movie.title}
-            />
-          </div>
-          <div>
-            <h1>{movie.title}</h1>
-            <div className={styles.genresContainer}>
-              <p>{movie.release_date.split("-")[0]}</p>
-              <p>{Math.floor(movie.runtime / 60)}h {movie.runtime % 60}m</p>
-              {movie.genres.map((genre: { id: number; name: string }) => (
-                <a className={styles.genreLink} key={genre.id} href={`/categories/${slugify(genre.name, { lower: true })}`}>{genre.name}</a>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className={styles.movieBackgroundImg}>
+      <div className={styles.movieHeader} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})` }}>
+        <div className={styles.moviePoster}>
           <img
-            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
             alt={movie.title}
+            title={movie.title}
           />
+        </div>
+        <div className={styles.moviePageInfo}>
+          <h1>{movie.title}</h1>
+          <div className={styles.genresContainer}>
+            <p>{movie.release_date.split("-")[0]}</p>
+            <p>{Math.floor(movie.runtime / 60)}h {movie.runtime % 60}m</p>
+            {movie.genres.map((genre: { id: number; name: string }) => (
+              <a className={styles.genreLink} key={genre.id} href={`/categories/${slugify(genre.name, { lower: true })}`}>{genre.name}</a>
+            ))}
+          </div>
+          <div className={styles.movieRating}>
+            <Image src={tmdbLogo} alt="TMDB Logo" width={20} height={20} title={`${rating} audience rating on TMDB`}/>
+            <p className={styles.voteAverage}>{rating}</p>
+          </div>
+          <p className={``}>{movie.overview}</p>
+          <div className={styles.externalLinks}>
+            <h1>External Links:</h1>
+            <Link className={`${styles.homePageIcon}`} href={movie.homepage} target="_blank" title="Official Site">
+              <LanguageIcon />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
