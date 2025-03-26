0c3bf8703d46f4057e6fa2fed5c7dca49a40b61f
josephr04 <jrosas.dev@gmail.com>
2025-03-25 23:16:07 -0500

    Fixing import paths

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import styles from '@styles/page.module.css';
-import Link from 'next/link'
+import Link from 'next/link';
 import slugify from 'slugify';
 import { Ripple } from 'primereact/ripple';
 
