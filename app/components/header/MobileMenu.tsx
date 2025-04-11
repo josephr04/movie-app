3f978878d339fdee975893fe0cd85fd256cf97ea
josephr04 <jrosas.dev@gmail.com>
2025-04-10 23:06:34 -0500

    Create mobile menu component

---
+++ app/components/header/MobileMenu.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useState, useEffect, useRef } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { NavList } from './NavList';
+import styles from '@styles/page.module.css';
+
+export function MobileMenu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const options = [
+    {
+      name: 'Home',
+      section: '#home-section'
+    },
+    {
+      name: 'Albums',
+      section: '#album-section'
+    },
+    {
+      name: 'Artists',
+      section: '#artist-section',
+    }
+  ];
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, [menuOpen]);
+
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  
+    return () => document.body.classList.remove('no-scroll');
+  }, [menuOpen]);
+
+  return (
+    <>
+      <div className={`${styles.menuIcon} ${menuOpen ? `${styles.menuIconActive}` : ``}`}>
+        <MenuIcon sx={{fontSize: 32 }} onClick={toggleMenu}/>
+      </div>
+
+      <div ref={menuRef}  className={`${styles.mobileMenu} ${menuOpen ? `${styles.mobileMenuOpen}` : `${styles.mobileMenuClosed}`}`}>
+        <div className={styles.menuSection}>
+          <NavList />
+        </div>
+      </div>
+    </>
+  )
+}
+
