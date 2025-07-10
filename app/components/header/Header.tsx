bf369b2d35ec13aa5e9ad0f793316590c1f2a221
josephr04 <jrosas.dev@gmail.com>
2025-07-10 17:34:08 -0500

    Add mobile menu component

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -3,16 +3,20 @@ import Link from 'next/link';
 import { Search } from 'lucide-react';
 import { DynamicHeader } from '@components/Animations';
 import { NavList } from './NavList';
+import { MobileMenu } from './MobileMenu';
 import { Ripple } from 'primereact/ripple';
 import styles from '@styles/page.module.css';
 
 function Header() {
   return (
     <DynamicHeader>
+      <div className={styles.mobileMenuComponent}>
+        <MobileMenu />
+      </div>
       <Link href="/" className={styles.logoLink} id={styles.test}>
-        <h1 className={styles.logo} >Movies+</h1>
+        <h1 className={styles.logo}>Movies+</h1>
       </Link>
-      <div className={styles.navList}>
+      <div className={styles.navListDesk}>
         <NavList />
       </div>
       <Link href="/search" className={`${styles.search} p-ripple`}>
