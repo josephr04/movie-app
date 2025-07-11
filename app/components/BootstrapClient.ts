4a75d8ed39c7c6e49f6f549f8f0c664a22fc150d
josephr04 <jrosas.dev@gmail.com>
2025-07-10 21:27:20 -0500

    Fix import in BoostrapClient component

--- app/components/BootstrapClient.ts
+++ app/components/BootstrapClient.ts
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import { useEffect } from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function BootstrapClient() {
   useEffect(() => {
-    require('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
 
   return null;
